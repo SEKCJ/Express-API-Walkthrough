8202bff5b482d068ee8244211d2f91150ff57fa8
SEKCJ <caesar@ea-vr.org>
2020-03-06 09:27:00 -0600

    walkthrough

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const apiRoutes = require('./routes')
+
+let app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', apiRoutes)
+
+
+app.listen(3000)
